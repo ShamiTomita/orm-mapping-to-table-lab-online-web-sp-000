4b3cd496d8813b0e7e2eb770e1d1a711a170581c
Shami Tomita <shamitomita@gmail.com>
2021-03-29 21:41:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,6 +21,17 @@ attr_accessor :name, :grade
   def self.drop_table 
     sql = <<-SQL
     DROP TABLE students
-    SQL 
+    SQL
+    
     DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+    INSERT INTO students(name, grade)
+    VALUES (?,?)
+    SQL
+    
+    DB[:conn].execute("SELECT last_insert_rowid()FROM students [0][0]")
+  end 
 end
