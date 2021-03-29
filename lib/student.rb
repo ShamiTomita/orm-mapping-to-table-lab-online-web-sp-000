225d8fc672c332931b85b3d95345b56502670473
Shami Tomita <shamitomita@gmail.com>
2021-03-29 21:43:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,8 @@ attr_accessor :name, :grade
     VALUES (?,?)
     SQL
     
-    DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0][0])
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0])
+    
   end 
 end
